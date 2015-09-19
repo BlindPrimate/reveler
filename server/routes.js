0886e1e5d364c4b080bdd9d05868aeb39b9aa0a2
BlindPrimate <ecleek@gmail.com>
2015-09-19 14:51:59 -0400

    fixed mobile display issues

--- server/routes.js
+++ server/routes.js
@@ -11,8 +11,7 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/revels', require('./api/revel'));
-  app.use('/api/things', require('./api/thing'));
-  app.use('/api/users', require('./api/user'));
+
 
   app.use('/auth', require('./auth'));
   
